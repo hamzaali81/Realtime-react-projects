0bb01d566ce544d03e6fe48da3da9b4e6a1906c8
hamzaali <hamza54834@gmail.com>
2020-04-26 11:48:27 -0700

    Mini book project

---
+++ my-app/src/App.js
@@ -0,0 +1,34 @@
+// import React, { Component } from 'react'
+// import logo from './logo.svg'
+// import './App.css'
+// // import greetings from './index.js'
+
+
+
+// class App extends Component {
+// render(){
+// var a="hello world my first project"
+//   return (
+//     <div className='App'>
+
+//       {/* <header className='App-header'>
+//         <img src={logo} className='App-logo' alt='logo' />
+//         <p> Hello world </p>
+//         <a
+//           className='App-link'
+//           href='https://reactjs.org'
+//           target='_blank'
+//           rel='noopener noreferrer'
+//         ></a>
+//       </header> */}
+  
+//       <h2>{a}</h2>
+//       <h2>Pakistan zindabad</h2>
+//       <h2><q>Ahmed</q></h2>
+//     </div>
+//   );
+// }
+// }
+
+
+// export default App;
---
+++ my-app/src/index.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css'
+
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+
+//stateless functional component
+//always return JSX
+
+
+//Es6 Nested Component and react tools
+// function Books(){
+//    return (
+  
+// <section className="books">
+// <Book />
+//   <Book />
+//   <Book />
+//   <Book />
+//   <Book />
+
+// </section>
+  
+
+
+//    );
+
+//   //  const Person = () => <h2>Hamza</h2>;
+  
+// }
+// // function Greetings(){
+// //   return React.createElement('h1',{},"hello world");
+// // }
+// // const Person = () => <h2>Hamza</h2>;
+// // const Message = () => <p>Hamza</p>;
+
+// const Book=()=>{
+//  return (<article className="book">
+//  <CoverImage />
+//  <Title />
+//   <Author />
+//  </article>)
+// }
+// const CoverImage=()=>(
+//   <img src="https://images-na.ssl-images-amazon.com/images/I/81TBdGvgJtL._AC_UL200_SR200,200_.jpg" alt="kids book" />
+// )
+
+// const Title=()=> <h1 style={{ fontSize: "2rem", color: "green"}}>Big Kindergarten</h1>;
+// const authorStyling={
+//   letterSpacing: "10px",
+//   color:"red"
+// };
+
+// const Author=()=> <p style={authorStyling}>by Craig & Smith</p>
+
+
+//three arguments elements,prosObject, children
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<Person />,document.getElementById('root'));
